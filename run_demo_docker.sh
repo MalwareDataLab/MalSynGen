f49f171015ddfad7792d67499cc0d58dc0d5c417
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-07-05 09:46:30 -0300

    Update run_demo_docker.sh

--- run_demo_docker.sh
+++ run_demo_docker.sh
@@ -3,5 +3,5 @@
 DIR=$(readlink -f shared)
 docker build -t sf24/syntabdata:latest . 
 
-sudo docker run -it --name=droidaugmentor-$RANDOM -v $DIR:/droidaugmentor/shared -e DISPLAY=unix$DISPLAY sf23/droidaugmentor:latest /droidaugmentor/shared/app_run.sh --verbosity 20 --output_dir /droidaugmentor/shared/outputs --input_dataset /droidaugmentor/shared/datasets/drebin215_small_64Malwares_64Benign.csv --dense_layer_sizes_g 256 --dense_layer_sizes_d 256 --number_epochs 1000 --training_algorithm Adam
+sudo docker run -it --name=droidaugmentor-$RANDOM -v $DIR:/SynTabData/shared -e DISPLAY=unix$DISPLAY sf23/droidaugmentor:latest /droidaugmentor/shared/app_run.sh --verbosity 20 --output_dir /droidaugmentor/shared/outputs --input_dataset /droidaugmentor/shared/datasets/drebin215_small_64Malwares_64Benign.csv --dense_layer_sizes_g 256 --dense_layer_sizes_d 256 --number_epochs 1000 --training_algorithm Adam
 ls shared/outputs/
