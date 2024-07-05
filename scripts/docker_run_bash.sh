6c6de2c239b9a2621b31e7a5178e2273bb0e63f5
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-07-04 23:28:11 -0300

    Update docker_run_bash.sh

--- scripts/docker_run_bash.sh
+++ scripts/docker_run_bash.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 sudo docker run -it --name=syntabdata-$RANDOM -e DISPLAY=unix$DISPLAY sf24/syntabdata:latest bash
-#sudo docker run -it --name=droidaumentor-$RANDOM -e DISPLAY=unix$DISPLAY sf23/droidaugmentor:latest python3 /droidaugmentor/setup/main.py --xx --yy --zz --kk ls ls ls 
