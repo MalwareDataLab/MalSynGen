7d353d5bec3be1f6993fd6c2ca0cd8e580e8b9ce
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-07-04 23:27:57 -0300

    Update docker_run_shared_dir.sh

--- scripts/docker_run_shared_dir.sh
+++ scripts/docker_run_shared_dir.sh
@@ -4,5 +4,4 @@
 	echo " example: $0 ."
 	exit
 }
-#sudo docker run -it --name=droidaugmentor-$RANDOM -v $(readlink -f $1):/droidaugmentor/shared -e DISPLAY=unix$DISPLAY sf23/droidaugmentor:latest python3 /droidaugmentor/setup/main.py --xx --yy --zz --kk ls ls ls
-sudo docker run -it --name=droidaugmentor-$RANDOM -v $(readlink -f $1):/droidaugmentor/shared -e DISPLAY=unix$DISPLAY sf23/droidaugmentor:latest bash 
+sudo docker run -it --name=syntabdata-$RANDOM -v $(readlink -f $1):/droidaugmentor/shared -e DISPLAY=unix$DISPLAY sf24/syntabdata:latest bash 
