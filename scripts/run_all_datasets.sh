62769bb3c151392b75f66ed9e3dc0651a2288f87
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-07-04 23:26:46 -0300

    Update run_all_datasets.sh

--- scripts/run_all_datasets.sh
+++ scripts/run_all_datasets.sh
@@ -3,5 +3,5 @@
 
 for DATASET in datasets/*
 do
-	pipenv run python generation.py --input_dataset  datasets/$DATASET --data_type float32 --num_samples_class_malware 64 --num_samples_class_benign 64 --number_epochs 1000 --classifier knn --k_fold 5 --latent_dimension 128 --output_dataset outputs/synthetic_dataset_$DATASET 
+	pipenv run python generation.py --input_dataset  datasets/$DATASET --data_type float32 --num_samples_class_malware 10000 --num_samples_class_benign 10000 --number_epochs 1000 --classifier knn --k_fold 5 --latent_dimension 128 --output_dataset outputs/synthetic_dataset_$DATASET 
 done
