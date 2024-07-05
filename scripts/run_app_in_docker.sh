59a7c7859877f14a8df248b435b0105b5b026a6c
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-07-04 22:56:16 -0300

    Update run_app_in_docker.sh

--- scripts/run_app_in_docker.sh
+++ scripts/run_app_in_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /droidaugmentor/
-pipenv run python main.py --verbosity 20 --output_dir outputs --input_dataset shared/datasets/drebin215_small_64Malwares_64Benign.csv --dense_layer_sizes_g 256 --dense_layer_sizes_d 256 --number_epochs 1000 --training_algorithm Adam
+pipenv run python3 main.py -i /datasets/kronodroid_real_device-balanced.csv  --num_samples_class_benign 10000 --num_samples_class_malware 10000 --batch_size 256 --number_epochs 500 --dense_layer_sizes_g 4096 --dense_layer_sizes_d 2048 --k_fold 10
 bash
