4a41a93eb873065e634fc3b0075a8349dba06174
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-07-05 10:36:37 -0300

    Update run_demo_docker.sh

--- run_demo_docker.sh
+++ run_demo_docker.sh
@@ -2,6 +2,13 @@
 #USER_ID=$(id -u $USER)
 DIR=$(readlink -f shared)
 docker build -t sf24/syntabdata:latest . 
+sudo docker run -it --name=syntabdata-$RANDOM -v $DIR:/SynTabData/shared -e DISPLAY=unix$DISPLAY sf24/syntabdata:latest /SynTabData/shared/app_run.sh --verbosity 20 --output_dir /SynTabData/shared/datasets/kronodroid_emulador-balanced.csv --num_samples_class_malware  10000 --num_samples_class_benign 10000 --dense_layer_sizes_g 4096 --dense_layer_sizes_d 2048 --number_epochs 1000 --k_fold 10 --training_algorithm Adam
+#ls shared/outputs/
 
-sudo docker run -it --name=droidaugmentor-$RANDOM -v $DIR:/SynTabData/shared -e DISPLAY=unix$DISPLAY sf23/droidaugmentor:latest /droidaugmentor/shared/app_run.sh --verbosity 20 --output_dir /droidaugmentor/shared/outputs --input_dataset /droidaugmentor/shared/datasets/drebin215_small_64Malwares_64Benign.csv --dense_layer_sizes_g 256 --dense_layer_sizes_d 256 --number_epochs 1000 --training_algorithm Adam
-ls shared/outputs/
+
+
+
+
+
+
+
