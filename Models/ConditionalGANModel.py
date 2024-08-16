0b03e981b4e10acd9c5f7ba6ea268a014619218c
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-08-16 08:40:24 -0300

    Add files via upload

--- Models/ConditionalGANModel.py
+++ Models/ConditionalGANModel.py
@@ -1,68 +1,45 @@
-"""
-Este módulo define a classe ConditionalGAN  responsavel por configurar e criar um modelo GAN condicional (cGAN) utilizando Keras.
-Classes: 
-    - Classe responsavel por configurar e criar um modelo GAN condicional utilizando Keras. 
-"""
-## Importação de bibliotecas necessárias
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'unknown'
+__email__ = 'unknown@unknown.com.br'
+__version__ = '{1}.{0}.{0}'
+__initial_data__ = '2022/06/01'
+__last_update__ = '2023/08/03'
+__credits__ = ['unknown']
+
 import numpy as np
-from keras.layers import Input, Dense, Flatten, Dropout, Activation, LeakyReLU, PReLU, Concatenate, BatchNormalization
+
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.layers import Activation
+from keras.layers import LeakyReLU
+from keras.layers import PReLU
+from keras.layers import Concatenate
+from keras.layers import BatchNormalization
 from keras.initializers import RandomNormal
 from keras.models import Model
 
-## Parâmetros padrão para a Conditional GAN
-##Valor padrão para o valor da minesão latente
+
 DEFAULT_CONDITIONAL_GAN_LATENT_DIMENSION = 128
-##Algoritmo padrão de treinamento para a cGAN
 DEFAULT_CONDITIONAL_GAN_TRAINING_ALGORITHM = "Adam"
-##Função de ativação padrão para a cGAN
 DEFAULT_CONDITIONAL_GAN_ACTIVATION = "LeakyReLU"
-##Valor padrão para a taxa de dropout para o gerador
 DEFAULT_CONDITIONAL_GAN_DROPOUT_DECAY_RATE_G = 0.2
-##Valor padrão para a taxa de dropout para o discriminador
 DEFAULT_CONDITIONAL_GAN_DROPOUT_DECAY_RATE_D = 0.4
-##Valor padrão para o tamanho de lote
 DEFAULT_CONDITIONAL_GAN_BATCH_SIZE = 32
-##Valor padrão para o número de classes
 DEFAULT_CONDITIONAL_GAN_NUMBER_CLASSES = 2
-##Valor padrão para o tamanho das camadas densas do gerador.
 DEFAULT_CONDITIONAL_GAN_DENSE_LAYERS_SETTINGS_G = [128]
-##Valor padrão para o tamanho das camadas densas do discriminador.
 DEFAULT_CONDITIONAL_GAN_DENSE_LAYERS_SETTINGS_D = [128]
-##Função padrão de perda da GAN
 DEFAULT_CONDITIONAL_GAN_LOSS = "binary_crossentropy"
-##Valor padrão para o momentum da GAN
 DEFAULT_CONDITIONAL_GAN_MOMENTUM = 0.8
-##Função de ativação padrão para a ultima camada
 DEFAULT_CONDITIONAL_LAST_ACTIVATION_LAYER = "sigmoid"
-##Valor padrão para a inicialização dos pesos das camadas
 DEFAULT_CONDITIONAL_GAN_INITIALIZER_MEAN = 0.0
-##Valor padrão para  o desvio padrão para inicialização dos pesos das camadas
 DEFAULT_CONDITIONAL_GAN_INITIALIZER_DEVIATION = 0.02
 
 
 class ConditionalGAN:
-    """
-    Classe responsavel por configurar e criar um modelo GAN condicional (cGAN) utilizando Keras.
-    Funções:
-        - __init__ : Inicializa a ConditionalGAN com os parâmetros especificados ou padrão.
-        - add_activation_layer :  Define a funlção de ativação especificada à  camada na rede neural.
-        - get_generator : Instância e retorna o modelo gerador da ConditionalGAN.
-        - get_discriminator : Instância e retorna o modelo gerador da ConditionalGAN.
-        - get_dense_generator_model : Retorna o modelo do gerador da ConditionalGAN.
-        - get_dense_discriminator_model :  Retorna o modelo do discriminador da ConditionalGAN.
-        - set_latent_dimension : Define a dimensão latente da ConditionalGAN.
-        - set_output_shape : Define o formato de saída da ConditionalGAN.
-        - set_activation_function : Define a função de ativação das camadas internas da ConditionalGAN.
-        - set_last_layer_activation :Define a função de ativação para a última camada do gerador.
-        - set_dropout_decay_rate_generator : Define a taxa do dropout para o gerador da ConditionalGAN.
-        - set_dropout_decay_rate_discriminator : Define a taxa do dropout para o discriminador da ConditionalGAN.
-        - set_dense_layer_sizes_generator : Define os tamanhos das camadas densas para o gerador da ConditionalGAN.
-        - set_dense_layer_sizes_discriminator : Define os tamanhos das camadas densas para o discriminador da ConditionalGAN.
-        - set_dataset_type : Define o tipo de dados do dataset.
-        - set_initializer_mean : Define a média para a inicialização dos pesos das camadas.
-        - set_initializer_deviation : Define o desvio padrão para inicialização dos pesos das camadas.
-    """
-
     def __init__(self, latent_dim=DEFAULT_CONDITIONAL_GAN_LATENT_DIMENSION, output_shape=None,
                  activation_function=DEFAULT_CONDITIONAL_GAN_ACTIVATION,
                  initializer_mean=DEFAULT_CONDITIONAL_GAN_INITIALIZER_MEAN,
@@ -71,22 +48,7 @@ class ConditionalGAN:
                  dropout_decay_rate_d=DEFAULT_CONDITIONAL_GAN_DROPOUT_DECAY_RATE_D,
                  last_layer_activation=DEFAULT_CONDITIONAL_LAST_ACTIVATION_LAYER,
                  dense_layer_sizes_g=None, dense_layer_sizes_d=None, dataset_type=np.float32):
-        """
-        Inicializa a ConditionalGAN com os parâmetros especificados ou padrão.
 
-        Parâmetros:
-           - latent_dim : Dimensão do espaço latente.
-           - output_shape t: Formato da saída.
-           - activation_function : Função de ativação para as camadas internas da cGAN.
-           - initializer_mean : Média para inicialização dos pesos das camadas
-           - initializer_deviation : Desvio padrão para inicialização dos pesos das camadas.
-           - dropout_decay_rate_g : Taxa de dropout para o gerador.
-           - dropout_decay_rate_d : Taxa de dropout para o discriminador.
-           - last_layer_activation : Função de ativação para a última camada do gerador.
-           - dense_layer_sizes_g : Tamanhos das camadas densas do gerador.
-           - dense_layer_sizes_d : Tamanhos das camadas densas do discriminador.
-           - dataset_type : Tipo de dados do dataset.
-        """
         if dense_layer_sizes_d is None:
             dense_layer_sizes_d = DEFAULT_CONDITIONAL_GAN_DENSE_LAYERS_SETTINGS_D
 
@@ -108,15 +70,7 @@ class ConditionalGAN:
         self.discriminator_model_dense = None
 
     def add_activation_layer(self, neural_nodel):
-        """
-        Define a funlção de ativação especificada à  camada na rede neural.
 
-        Parâmetros:
-           - neural_nodel : Camada da rede neural.
-
-        Retorno:
-           - neural_nodel: Camada de rede neural com função de ativação adicionada.
-        """
         if self.activation_function == 'LeakyReLU':
             neural_nodel = LeakyReLU()(neural_nodel)
 
@@ -129,182 +83,99 @@ class ConditionalGAN:
         return neural_nodel
 
     def get_generator(self):
-        """
-        Instância e retorna o modelo gerador da ConditionalGAN.
 
-         Retorno:
-           - Model: Modelo do gerador.
-        """
         initialization = RandomNormal(mean=self.initializer_mean, stddev=self.initializer_deviation)
         neural_model_inputs = Input(shape=(self.latent_dim,), dtype=self.dataset_type)
+        latent_input = Input(shape=(self.latent_dim,))
+
+        label_input = Input(shape=(1,), dtype=self.dataset_type)
 
         generator_model = Dense(self.dense_layer_sizes_g[0], kernel_initializer=initialization)(neural_model_inputs)
+        #generator_model = BatchNormalization(momentum=0.8)(generator_model)
         generator_model = Dropout(self.dropout_decay_rate_g)(generator_model)
         generator_model = self.add_activation_layer(generator_model)
 
         for layer_size in self.dense_layer_sizes_g[1:]:
             generator_model = Dense(layer_size, kernel_initializer=initialization)(generator_model)
+            #generator_model = BatchNormalization(momentum=0.8)(generator_model)
             generator_model = Dropout(self.dropout_decay_rate_g)(generator_model)
             generator_model = self.add_activation_layer(generator_model)
 
-        generator_model = Dense(self.output_shape, self.last_layer_activation, kernel_initializer=initialization)(
+        generator_model = Dense(self.output_shape,activation='relu', kernel_initializer=initialization)(
             generator_model)
         generator_model = Model(neural_model_inputs, generator_model, name="Dense_Generator")
         self.generator_model_dense = generator_model
-
-        latent_input = Input(shape=(self.latent_dim,))
-        label_input = Input(shape=(1,), dtype=self.dataset_type)
         concatenate_output = Concatenate()([latent_input, label_input])
         label_embedding = Flatten()(concatenate_output)
         model_input = Dense(self.latent_dim)(label_embedding)
+        model_input = self.add_activation_layer(model_input)
         generator_output_flow = generator_model(model_input)
 
         return Model([latent_input, label_input], generator_output_flow, name="Generator")
 
     def get_discriminator(self):
-        """
-        Instância e retorna o modelo discriminador da ConditionalGAN.
 
-        Retorno:
-           - Model: Modelo do discriminador.
-        """
-        neural_model_input = Input(shape=(self.output_shape,), dtype=self.dataset_type)
-        discriminator_model = Dense(self.dense_layer_sizes_d[0])(neural_model_input)
+        neural_model_input = Input(shape=(self.output_shape//16,), dtype=self.dataset_type)
+        discriminator_shape_input = Input(shape=(self.output_shape,))
+        label_input = Input(shape=(1,), dtype=self.dataset_type)
+
+        discriminator_model = Dense(self.dense_layer_sizes_d[0]//2,name="pesado")(neural_model_input)
         discriminator_model = Dropout(self.dropout_decay_rate_d)(discriminator_model)
+        #discriminator_model = BatchNormalization(momentum=0.8)(discriminator_model)
         discriminator_model = self.add_activation_layer(discriminator_model)
 
         for layer_size in self.dense_layer_sizes_d[1:]:
-            discriminator_model = Dense(layer_size)(discriminator_model)
+
+            discriminator_model = Dense(layer_size//2,)(discriminator_model)
+            #discriminator_model = BatchNormalization(momentum=0.8)(discriminator_model)
             discriminator_model = Dropout(self.dropout_decay_rate_d)(discriminator_model)
             discriminator_model = self.add_activation_layer(discriminator_model)
 
-        discriminator_model = Dense(1, self.last_layer_activation)(discriminator_model)
-        discriminator_model = Model(inputs=neural_model_input, outputs=discriminator_model, name="Dense_Discriminator")
-
+        discriminator_model = Dense(1, self.last_layer_activation,name='primeiro_neuronio')(discriminator_model)
+        discriminator_model = Model(inputs=neural_model_input, outputs=discriminator_model, name="segundo_neuronio")
         self.discriminator_model_dense = discriminator_model
-
-        discriminator_shape_input = Input(shape=(self.output_shape,))
-        label_input = Input(shape=(1,), dtype=self.dataset_type)
         concatenate_output = Concatenate()([discriminator_shape_input, label_input])
         label_embedding = Flatten()(concatenate_output)
-        model_input = Dense(self.output_shape)(label_embedding)
+        model_input = Dense(self.output_shape//16)(label_embedding)
+        model_input = self.add_activation_layer(model_input)
         validity = discriminator_model(model_input)
-
-        return Model(inputs=[discriminator_shape_input, label_input], outputs=validity, name="Discriminator")
+        return Model(inputs=[discriminator_shape_input, label_input], outputs=validity, name='Discriminator')
 
     def get_dense_generator_model(self):
-        """
-        Retorna o modelo do gerador da ConditionalGAN.
-
-        Retorno:
-           - self.generator_model_dense: Modelo do gerador.
-        """
         return self.generator_model_dense
 
     def get_dense_discriminator_model(self):
-        """
-        Retorna o modelo do discriminador da ConditionalGAN.
-
-        Retorno:
-           - self.discriminator_model_dense: Modelo do discriminador.
-        """
         return self.discriminator_model_dense
 
     def set_latent_dimension(self, latent_dimension):
-        """
-        Define a dimensão latente da ConditionalGAN.
-
-        Parâmetros:
-           - latent_dimension : Dimensão latente.
-        """
         self.latent_dim = latent_dimension
 
     def set_output_shape(self, output_shape):
-        """
-        Define o formato de saída da ConditionalGAN.
-
-        Parâmetros:
-          -  output_shape : Formato de saída da ConditionalGAN.
-        """
         self.output_shape = output_shape
 
     def set_activation_function(self, activation_function):
-        """
-        Define a função de ativação das camadas internas da ConditionalGAN.
-
-        Parâmetros:
-          -  activation_function: Função de ativação.
-        """
         self.activation_function = activation_function
 
     def set_last_layer_activation(self, last_layer_activation):
-        """
-        Define a função de ativação para a última camada do gerador.
-
-        Parâmetros:
-          -  last_layer_activation: Função de ativação para a última camada do gerador.
-        """
         self.last_layer_activation = last_layer_activation
 
     def set_dropout_decay_rate_generator(self, dropout_decay_rate_generator):
-        """
-        Define a taxa do dropout para o gerador da ConditionalGAN.
-
-        Parâmetros:
-          -  dropout_decay_rate_generator: Taxa  do dropout para o gerador.
-        """
         self.dropout_decay_rate_g = dropout_decay_rate_generator
 
     def set_dropout_decay_rate_discriminator(self, dropout_decay_rate_discriminator):
-        """
-        Define a taxa do dropout para o discriminador da ConditionalGAN.
-
-        Parâmetros:
-          -  dropout_decay_rate_discriminator: Taxa do dropout para o discriminador.
-        """
         self.dropout_decay_rate_d = dropout_decay_rate_discriminator
 
     def set_dense_layer_sizes_generator(self, dense_layer_sizes_generator):
-        """
-        Define os tamanhos das camadas densas para o gerador da ConditionalGAN.
-
-        Parâmetros:
-           - dense_layer_sizes_generator: Lista de tamanhos das camadas densas para o gerador.
-        """
         self.dense_layer_sizes_g = dense_layer_sizes_generator
 
     def set_dense_layer_sizes_discriminator(self, dense_layer_sizes_discriminator):
-        """
-        Define os tamanhos das camadas densas para o discriminador da ConditionalGAN.
-
-        Parâmetros:
-           - dense_layer_sizes_discriminator : Lista de tamanhos das camadas densas para o discriminador.
-        """
         self.dense_layer_sizes_d = dense_layer_sizes_discriminator
 
     def set_dataset_type(self, dataset_type):
-        """
-        Define o tipo de dados do dataset.
-
-        Parâmetros:
-          -  dataset_type : Tipo de dado do dataset.
-        """
         self.dataset_type = dataset_type
 
     def set_initializer_mean(self, initializer_mean):
-        """
-        Define a média para a inicialização dos pesos das camadas.
-
-        Parâmetros:
-          -  initializer_mean : média para inicialização dos pesos das camadas.
-        """
         self.initializer_mean = initializer_mean
 
     def set_initializer_deviation(self, initializer_deviation):
-        """
-        Define o desvio padrão para inicialização dos pesos das camadas.
-
-        Parâmetros:
-           - initializer_deviation : desvio padrão para inicialização dos pesos das camadas.
-        """
         self.initializer_deviation = initializer_deviation
--- Models/PerceptronModel.py
+++ Models/PerceptronModel.py
@@ -1,73 +1,37 @@
-"""
-Este módulo define a classe PerceptronMutlilayer e suas funções utilizadas para a classificação de dados binarios
-Classes:
-    -PerceptronMultilayer (Responsável pelo instanciamento e criação de um perceptron de múltiplas camadas(MLP))
-"""
-# Importação de bibliotecas necessárias
-from keras.layers import Dense, Dropout, Input
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'unknown'
+__email__ = 'unknown@unknown.com.br'
+__version__ = '{1}.{0}.{0}'
+__initial_data__ = '2022/06/01'
+__last_update__ = '2023/08/03'
+__credits__ = ['unknown']
+
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Input
 from keras.models import Model
 from tensorflow import keras
-import numpy as np
+import numpy
 
-## Algoritmo padrão de treinamento
 DEFAULT_PERCEPTRON_TRAINING_ALGORITHM = "Adam"
-## Função de perda padrão para o perceptron
 DEFAULT_PERCEPTRON_LOSS = "binary_crossentropy"
-## Configuração de camadas padrão para o Perceptron
 DEFAULT_PERCEPTRON_LAYERS_SETTINGS = [512, 256, 256]
-## Valor padrão para a taxa de dropouut
 DEFAULT_PERCEPTRON_DROPOUT_DECAY_RATE = 0.2
-## Métricas padrão utilizada para a avaliação do modelo.
 DEFAULT_PERCEPTRON_METRIC = ["accuracy"]
-## Função padrão de ativação para as camadas internas do modelo
 DEFAULT_PERCEPTRON_LAYER_ACTIVATION = keras.activations.swish
-## Função padrão de ativação para a última camada do modelo
 DEFAULT_PERCEPTRON_LAST_LAYER_ACTIVATION = "sigmoid"
-## Valor padrão de entrada para o perceptron
-DEFAULT_PERCEPTRON_DATA_TYPE = np.float32
+DEFAULT_PERCEPTRON_DATA_TYPE = numpy.float32
 
 
 class PerceptronMultilayer:
-    """
-     A classe PerceptronMultilayer é responsável pelo instanciamento e criação de um perceptron de múltiplas camadas(MLP)
-     Funções:
-        - __init__ : Inicializa o PerceptronMultilayer com as configurações especificadas
-        - get_model :  Compila a instância do modelo com bases nas configuraçõe especificadas.
-        - set_training_algorithm : Defino o algoritmo de treinamento a ser utilizado para optimização durante o treinamento do modelo.
-        - set_training_loss : Define a função de perda a ser utilizada.
-        - set_data_type: Define o tipo de dados para a entrada do modelo.
-        - set_last_layer_activation :  Define a função de ativação para a última camada do modelo.
-        - set_dropout_decay_rate :  Define a taxa de dropout para o modelo
-
-    Parâmetros:
-        - training_algorithm : Algoritmo de treinamento a ser utilizado.
-        - training_loss : A função de perda utilizada.
-        - data_type :  O tipo de dados utilizada para a entrada do modelo.
-        - layer_activation : Função de ativação das camadas ocultas.
-        - last_layer_activation: Função de ativação para a última camada do modelo.
-        - dropout_decay_rate : Taxa de dropout.
-        - training_metric : Métricas utilizadas para a avaliação do modelo.
-        - layers_settings : Número de neurônios  por camada.
-    """
-
     def __init__(self, layers_settings=None, training_metric=None, training_loss=DEFAULT_PERCEPTRON_LOSS,
                  training_algorithm=DEFAULT_PERCEPTRON_TRAINING_ALGORITHM, data_type=DEFAULT_PERCEPTRON_DATA_TYPE,
                  layer_activation=DEFAULT_PERCEPTRON_LAYER_ACTIVATION,
                  last_layer_activation=DEFAULT_PERCEPTRON_LAST_LAYER_ACTIVATION,
                  dropout_decay_rate=DEFAULT_PERCEPTRON_DROPOUT_DECAY_RATE):
-        """
-        Inicializa o PerceptronMultilayer com as configurações especificadas.
 
-        Parâmetros:
-            - layers_settings : Número de neurônios  por camada.
-            - training_metric : Métricas utilizadas para a avaliação do modelo.
-            - training_loss : A função de perda utilizada
-            - training_algorithm : : Algoritmo de treinamento a ser utilizado.
-            - data_type : O tipo de dados utilizada para a entrada do modelo.
-            - layer_activation : Função de ativação das camadas ocultas.
-            - last_layer_activation : Função de ativação para a última camada do gerador .
-            - dropout_decay_rate : Taxa de dropout.
-        """
         self.training_algorithm = training_algorithm
         self.training_loss = training_loss
         self.data_type = data_type
@@ -85,84 +49,41 @@ class PerceptronMultilayer:
         self.layers_settings = layers_settings
 
     def get_model(self, input_shape):
-        """
-       Compila a instância do modelo com bases nas configuraçõe especificadas.
 
-        Parâmetros:
-            input_shape: Formato da entrada.
-
-        Retorna
-            keras.Model: Modelo Keras compilado representando o Perceptron.
-        """
         input_layer = Input(shape=input_shape, dtype=self.data_type)
-        dense_layer = Dense(self.layers_settings[0], activation=self.layer_activation)(input_layer)
 
-        for num_neurons in self.layers_settings[1:]:
-            dense_layer = Dense(num_neurons, activation=self.layer_activation)(dense_layer)
+        dense_layer = Dense(self.layers_settings[0], self.layer_activation)(input_layer)
+
+        for number_neurons in self.layers_settings[1:]:
+            dense_layer = Dense(number_neurons, self.layer_activation)(dense_layer)
             dense_layer = Dropout(self.dropout_decay_rate)(dense_layer)
 
-        output_layer = Dense(1, activation=self.last_layer_activation)(dense_layer)
+        dense_layer = Dense(1, self.last_layer_activation)(dense_layer)
+        neural_network_model = Model(input_layer, dense_layer)
+        neural_network_model.compile(self.training_algorithm, self.training_loss, self.training_metric)
 
-        perceptron_model = Model(input_layer, output_layer)
-        perceptron_model.compile(optimizer=self.training_algorithm,
-                                 loss=self.training_loss,
-                                 metrics=self.training_metric)
-
-        return perceptron_model
+        return neural_network_model
 
     def set_training_algorithm(self, training_algorithm):
-        """
-        Defino o algoritmo de treinamento a ser utilizado para optimização durante o treinamento do modelo.
 
-        Parâmetros:
-            training_algorithm : Algoritmo de treinamento a ser utilizado.
-        """
         self.training_algorithm = training_algorithm
 
     def set_training_loss(self, training_loss):
-        """
-        Define a função de perda a ser utilizada.
 
-        Parâmetros:
-            training_loss: Função da perda a ser utilizada.
-        """
         self.training_loss = training_loss
 
     def set_data_type(self, data_type):
-        """
-        Define o tipo de dados para a entrada do modelo.
 
-        Parâmetros:
-            data_type: Tipo de dados para a entrada do modelo.
-        """
         self.data_type = data_type
 
     def set_layer_activation(self, layer_activation):
-        """
-        Define a função de ativação para as camadas internas do modelo.
 
-        Parâmetros:
-            layer_activation : Função de ativação das camadas internas do modelo.
-        """
         self.layer_activation = layer_activation
 
     def set_last_layer_activation(self, last_layer_activation):
-        """
 
-        Define a função de ativação para a última camada do modelo.
-
-        Parâmetros:
-            last_layer_activation : Função de ativação para a última camada do modelo.
-        """
         self.last_layer_activation = last_layer_activation
 
     def set_dropout_decay_rate(self, dropout_decay_rate):
-        """
-        Define a taxa de dropout para o modelo
 
-        Parâmetros:
-
-            dropout_decay_rate : Taxa de dropout.
-        """
         self.dropout_decay_rate = dropout_decay_rate
-
